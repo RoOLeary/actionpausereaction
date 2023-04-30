b87910245f93a618815e21021ae54f18ee4c0dd3
Ronan <roleary81@gmail.com>
2023-04-30 13:40:34 +0200

    add soundcloud embed:

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,11 +7,14 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center py-24">
       <div className="z-10 max-w-7xl items-center justify-between font-mono sm:text-xs text-sm lg:flex-col">
-        <p className="font-mono font-bold text-center text-lg md:text-2xl">action | pause | reaction</p>
+        <p className="font-mono font-bold text-center text-lg md:text-4xl">action | pause | reaction</p>
         <br/>
         <p className="font-mono text-center text-sm">coming (back) again soon. stay tuned</p>
         <br/>
-        <p className="font-mono text-center text-sm">listen on <a href="https://soundcloud.com/actionpausereaction" target="_blank" rel="no-referrer">soundcloud</a></p>
+        <br />
+        <div className="py-8">
+          <iframe width="100%" height="350px" scrolling="no" frameBorder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/2579536&color=%23ff5500&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe>
+        </div>
       </div>
 
       
