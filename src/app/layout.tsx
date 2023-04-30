1bdcbaa1618f4403bc59bc6dea704f69dc0a2352
Ronan <roleary81@gmail.com>
2023-04-30 13:28:50 +0200

    new fav and stuff. test next13

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import './globals.css'
+import Head from 'next/head'
 import { Inter } from 'next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -6,7 +7,15 @@ const inter = Inter({ subsets: ['latin'] })
 export const metadata = {
   title: 'action | pause | reaction',
   description: 'Irish Alternative rock...from The Netherlands and the UK',
+  icons: {
+    icon: {
+      url: "/favicon.ico",
+      type: "image/png",
+    },
+    shortcut: { url: "/favicon.ico", type: "image/png" },
+  }
 }
+
 
 export default function RootLayout({
   children,
